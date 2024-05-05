ee80629be377aa4860c92d0e1c30b4a59eb056f8
Simon-Sandrew <sandrewsimon@gmail.com>
2024-05-05 00:50:00 -0400

    changes so we can usepostman

--- main.py
+++ main.py
@@ -38,20 +38,20 @@ NUMBER_OF_CALLS = 1 # global var for number of pharmacies we will call
 @functions_framework.http
 def main(request):
     
-    # Set CORS headers for the preflight request
-    if request.method == "OPTIONS":
-        # Allows GET requests from any origin with the Content-Type
-        # header and caches preflight response for an 3600s
-        headers = {
-            "Access-Control-Allow-Origin": "*", # change from "*" to "https://rx-radar.com" for production
-            "Access-Control-Allow-Methods": "POST, OPTIONS",
-            "Access-Control-Allow-Headers": "Content-Type",
-            "Access-Control-Max-Age": "3600",
-        }
+    # # Set CORS headers for the preflight request
+    # if request.method == "OPTIONS":
+    #     # Allows GET requests from any origin with the Content-Type
+    #     # header and caches preflight response for an 3600s
+    #     headers = {
+    #         "Access-Control-Allow-Origin": "*", # change from "*" to "https://rx-radar.com" for production
+    #         "Access-Control-Allow-Methods": "POST, OPTIONS",
+    #         "Access-Control-Allow-Headers": "Content-Type",
+    #         "Access-Control-Max-Age": "3600",
+    #     }
 
-        return ("", 204, headers)
+    #     return ("", 204, headers)
 
-    # Set CORS headers for the main request
+    # # Set CORS headers for the main request
     headers = {"Access-Control-Allow-Origin": "*"} # change from "*" to "https://rx-radar.com" for production
 
     # begin the actual medication_request
