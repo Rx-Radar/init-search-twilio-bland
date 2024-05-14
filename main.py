a939141f39ed8b97d46273a69460d67f447de8bd
Simon-Sandrew <sandrewsimon@gmail.com>
2024-05-13 23:37:22 -0400

    doubleValue

--- main.py
+++ main.py
@@ -67,8 +67,8 @@ def main(cloud_event: CloudEvent):
     
     prescription = firestore_dict["prescription"]["mapValue"]["fields"]
     user_location = firestore_dict["user_location"]["mapValue"]["fields"]
-    lat = user_location["lat"]["stringValue"]
-    lon = user_location["lon"]["stringValue"]
+    lat = user_location["lat"]["doubleValue"]
+    lon = user_location["lon"]["doubleValue"]
     
     user_uuid = firestore_dict["user_uuid"]["stringValue"]
 
